4bf2f16679abab02740a366b9ab52e28489f1ec3
bencohen694 <79834943+bencohen694@users.noreply.github.com>
2021-03-11 16:03:28 +0200

    Update first_pass.c

--- first_pass.c
+++ first_pass.c
@@ -225,18 +225,19 @@ static void build_extra_codeword_fpass(machine_word **code_img, long *ic, char *
 			machine_word *word_to_write;
 			/* Get value of immediate addressed operand. notice that it starts with #, so we're skipping the # in the call to strtol */
 			long value = strtol(operand + 1, &ptr, 10);
+			word_to_write->ARE='A';
 			word_to_write = (machine_word *) malloc_with_check(sizeof(machine_word));
 			word_to_write->length = 0; /* Not Code word! */
 			(word_to_write->word).data = build_data_word(IMMEDIATE_ADDR, value, FALSE);
-
 			code_img[(*ic) - IC_INIT_VALUE] = word_to_write;
 		}
+		
 		else if (operand_addressing == REGISTER_ADDR) {
 			char *ptr;
-
 			machine_word *word_to_write;
 			/* Get value of immediate addressed operand. notice that it starts with #, so we're skipping the # in the call to strtol */
 			long value = strtol(2^(get_register_by_name(operand + 1)), &ptr, 10);
+			word_to_write->ARE='A';
 			word_to_write = (machine_word *) malloc_with_check(sizeof(machine_word));
 			word_to_write->length = 0; /* Not Code word! */
 			(word_to_write->word).data = build_data_word(REGISTER_ADDR, value, FALSE);
